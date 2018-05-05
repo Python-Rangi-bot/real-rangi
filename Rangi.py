cd3f370c775c2e365dff4cc3a600e7753857e6c7
kokbab88611 <38698459+kokbab88611@users.noreply.github.com>
2018-05-06 00:21:41 +0900

    Update Rangi.py

--- Rangi.py
+++ Rangi.py
@@ -7,11 +7,6 @@ import time
 import random
 import os
 
-#startup_extensions = ["Music"]
-bot = commands.Bot("")
-import Music as startup_extensions
-
-
 @bot.event
 async def on_ready():
     print("준비 되었느니라")
